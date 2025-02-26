bc96a51e034879c8989f23e6baf1c1c391a6892e
7x11x13 <x7x11x13@gmail.com>
2025-02-26 16:13:49 -0500

    Suspense

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -3,7 +3,8 @@ import Home from "./containers/Home.tsx";
 import NotFound from "./containers/NotFound.tsx";
 import AuthenticatedRoute from "./components/AuthenticatedRoute.tsx";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute.tsx";
-import React from "react";
+import React, { Suspense } from "react";
+import { Center, Loader } from "@mantine/core";
 
 const Pricing = React.lazy(() => import("./containers/Pricing.tsx"));
 const Upload = React.lazy(() => import("./containers/Upload.tsx"));
@@ -12,11 +13,13 @@ const Settings = React.lazy(() => import("./containers/Settings.tsx"));
 export default function Routes() {
     return (
         <Switch>
-            <Route path="/"><UnauthenticatedRoute to={"/upload"}><Home /></UnauthenticatedRoute></Route>
-            <Route path="/pricing"><Pricing /></Route>
-            <Route path="/upload"><AuthenticatedRoute><Upload /></AuthenticatedRoute></Route>
-            <Route path="/settings"><AuthenticatedRoute><Settings /></AuthenticatedRoute></Route>
-            <Route><NotFound /></Route>
+            <Suspense fallback={<Center><Loader /></Center>}>
+                <Route path="/"><UnauthenticatedRoute to={"/upload"}><Home /></UnauthenticatedRoute></Route>
+                <Route path="/pricing"><Pricing /></Route>
+                <Route path="/upload"><AuthenticatedRoute><Upload /></AuthenticatedRoute></Route>
+                <Route path="/settings"><AuthenticatedRoute><Settings /></AuthenticatedRoute></Route>
+                <Route><NotFound /></Route>
+            </Suspense>
         </Switch>
     );
 }
