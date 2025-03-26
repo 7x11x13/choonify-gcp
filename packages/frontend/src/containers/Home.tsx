3d803ff067a2e15e1828a6d88682864e35943ebc
7x11x13 <x7x11x13@gmail.com>
2025-03-25 21:55:32 -0400

    Add demo video to homepage

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -1,4 +1,4 @@
-import { Center, Stack, Text, Title } from "@mantine/core";
+import { Box, Stack, Text, Title } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 import GoogleLoginButton from "../components/GoogleLoginButton";
 
@@ -6,13 +6,33 @@ export default function Home() {
   const { t } = useTranslation();
 
   return (
-    <Center>
-      <Stack justify="center" gap="0" ta="center" align="center">
-        <Title>Choonify</Title>
-        <Text mb="lg">{t("slogan")}</Text>
-        <GoogleLoginButton></GoogleLoginButton>
-        {/* TODO: demo video embed */}
-      </Stack>
-    </Center>
+    <Stack justify="center" gap="0" ta="center" align="center">
+      <Title>Choonify</Title>
+      <Text mb="lg">{t("slogan")}</Text>
+      <GoogleLoginButton></GoogleLoginButton>
+      <Box w="100%" maw="900px" mt="50px">
+        <Box
+          pos="relative"
+          w="100%"
+          pb="56.25%" // 9/16 * 100% for 16:9 aspect ratio
+        >
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube-nocookie.com/embed/H24rG_eNp5U?si=LYASN6GiTbAPQzqT&cc_load_policy=1&rel=0"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              border: 0,
+            }}
+          />
+        </Box>
+      </Box>
+    </Stack>
   );
 }
