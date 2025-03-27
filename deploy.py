57167b9217e3b0aadd16480056091326dc96fcc9
7x11x13 <x7x11x13@gmail.com>
2025-03-27 10:16:12 -0400

    Disable ads for now

--- deploy.py
+++ deploy.py
@@ -92,7 +92,7 @@ def build_frontend(api_env: dict[str, str]):
         "VITE_LOCAL_BACKEND": "0",
         "VITE_GOOGLE_CLIENT_ID": api_env["GOOGLE_CLIENT_ID"],
         "VITE_FIREBASE_CONFIG": api_env["FIREBASE_CONFIG"],
-        "VITE_ENABLE_ADS": "1",
+        "VITE_ENABLE_ADS": "0",
     }
 
     with contextlib.chdir("./packages/frontend"):
